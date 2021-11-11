8af7dc3d8fa32eb89b913400e6e59b464ee0a311
jvbm07 <joaovitorbptm@gmail.com>
2021-11-11 15:47:29 -0300

    odds adicionadas nos confrontos

--- previa/src/aggregation/getPlayersMatchesSince2019.py
+++ previa/src/aggregation/getPlayersMatchesSince2019.py
@@ -4,12 +4,17 @@ import numpy as np
 matches2019 = pd.read_csv("../../data/external/mencsv/2019.csv")
 matches2020 = pd.read_csv("../../data/external/mencsv/2020.csv")
 matches2021 = pd.read_csv("../../data/external/mencsv/2021.csv")
+odds2019 = matches2019['AvgW'].astype(str) + ':' + matches2019['AvgL'].astype(str)
+odds2020 = matches2020['AvgW'].astype(str) + ':' + matches2020['AvgL'].astype(str)
+odds2021 = matches2021['AvgW'].astype(str) + ':' + matches2021['AvgL'].astype(str)
 players2019 = matches2019[['Winner','Loser']]
 players2020 = matches2020[['Winner','Loser']]
 players2021 = matches2021[['Winner','Loser']]
-
+odds_all = pd.concat([odds2019,odds2020,odds2021], ignore_index=True)
+odds_all.columns = ['Odds']
 players_all = pd.concat([players2019,players2020,players2021], ignore_index=True)
-players_all.rename(columns={'Winner':'Player1','Loser':'Player2'}, inplace=True)
+players_all = pd.concat([players_all,odds_all], ignore_index=True, axis = 1)
+players_all.columns=['Winner','Loser', 'Odds']
 players_all.to_csv('../../data/interim/players_since_2019.csv', index=False, encoding='utf-8')
 
 print(players_all)
